efac0776d1441d25b003bee25e198641c64b3648
taznimt <taznimanwar06@gmail.com>
2024-05-07 15:30:00 +0700

    endl

--- pointerdanfriendclass/pointerdanfriendclass2.cpp
+++ pointerdanfriendclass/pointerdanfriendclass2.cpp
@@ -12,4 +12,14 @@ public :
 };
 int main() {
 	mahasiswa mhs{ 1 }; // object mhs
+	mhs.showNim(); // member acces operator
+
+	mahasiswa& refMhs = mhs; //pointer reference refMhs
+	refMhs.nim = 2; // member access operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs; //pointer deference pMhs
+	pMhs->nim = 3; //Arrow operstor
+	pMhs->showNim();
+	return 0;
 }
