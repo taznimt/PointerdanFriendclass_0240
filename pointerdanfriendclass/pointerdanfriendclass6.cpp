3118e06f204cb51347f49312fee8e2e4b7c89983
taznimt <taznimanwar06@gmail.com>
2024-05-07 16:07:42 +0700

    create class pelajar

--- pointerdanfriendclass/pointerdanfriendclass6.cpp
+++ pointerdanfriendclass/pointerdanfriendclass6.cpp
@@ -4,6 +4,15 @@ class pelajar;
 class manusia
 {
 public:
-	void showNilaiPelajar(pelajar& x);
+	void showNilaiPelajar(pelajar &x);
 
+};
+class pelajar
+{
+private:
+	int nilai;
+
+public:
+	pelajar() { nilai = 100; }
+	friend void manusia::showNilaiPelajar(pelajar& x);
 };
