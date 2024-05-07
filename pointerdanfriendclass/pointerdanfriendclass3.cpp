0ede5ddd4878d267d6079b21e1c71fb7860faa14
taznimt <taznimanwar06@gmail.com>
2024-05-07 15:34:26 +0700

    implementasi pembuatan dan penghapusan objec pointer mahasiswa

---
+++ pointerdanfriendclass/pointerdanfriendclass3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa
+{
+public:
+    int nim;
+
+    void showNim()
+    {
+        cout << "No Induk = " << nim << endl;
+    }
+};
+
+int main()
+{
+    mahasiswa* mhs = new mahasiswa{ 1 }; // pointer object mhs
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
+}
